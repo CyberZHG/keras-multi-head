69c73a4b3a014d8d04e559af17213b2405854f7f
CyberZHG <CyberZHG@gmail.com>
2018-09-20 08:42:00 +0800

    Support multi-layer input

--- keras_multi_head/multi_head.py
+++ keras_multi_head/multi_head.py
@@ -7,23 +7,43 @@ class MultiHead(keras.layers.Wrapper):
 
     def __init__(self,
                  layer,
-                 layer_num,
+                 layer_num=1,
                  **kwargs):
         """Initialize the wrapper layer.
 
-        :param layer: The layer to be duplicated.
+        :param layer: The layer to be duplicated or a list of layers.
         :param layer_num: The number of duplicated layers.
         :param kwargs: Arguments for parent.
         """
-        self.layer = layer
-        self.layers = []
-        self.layer_num = layer_num
-        super(MultiHead, self).__init__(layer, **kwargs)
+        if type(layer) is list:
+            self.layer = layer[0]
+            self.layers = layer
+            self.layer_num = len(self.layers)
+            self.rename = False
+        else:
+            self.layer = layer
+            self.layers = []
+            self.layer_num = layer_num
+            self.rename = True
+        self.supports_masking = self.layer.supports_masking
+        super(MultiHead, self).__init__(self.layer, **kwargs)
 
     def get_config(self):
-        config = {'layer_num': self.layer_num}
+        config = {'layers': []}
+        for layer in self.layers:
+            config['layers'].append({
+                'class_name': layer.__class__.__name__,
+                'config': layer.get_config(),
+            })
         base_config = super(MultiHead, self).get_config()
+        base_config.pop('layer')
         return dict(list(base_config.items()) + list(config.items()))
+
+    @classmethod
+    def from_config(cls, config, custom_objects=None):
+        from keras.layers import deserialize as deserialize_layer
+        layers = [deserialize_layer(layer, custom_objects=custom_objects) for layer in config.pop('layers')]
+        return cls(layers, **config)
 
     def build(self, input_shape):
         if type(input_shape) == list:
@@ -31,9 +51,11 @@ class MultiHead(keras.layers.Wrapper):
         else:
             self.input_spec = keras.engine.InputSpec(shape=input_shape)
         if not self.layers:
-            self.layers = [copy.copy(self.layer) for _ in range(self.layer_num)]
-            for i, layer in enumerate(self.layers):
-                layer.name = layer.name + '_%d' % (i + 1)
+            self.layers = [copy.deepcopy(self.layer) for _ in range(self.layer_num)]
+        for i, layer in enumerate(self.layers):
+            if not layer.built:
+                if self.rename:
+                    layer.name = layer.name + '_%d' % (i + 1)
                 layer.build(input_shape)
         super(MultiHead, self).build(input_shape)
 
@@ -47,3 +69,68 @@ class MultiHead(keras.layers.Wrapper):
     def call(self, inputs, mask=None):
         outputs = [K.expand_dims(layer.call(inputs, mask)) for layer in self.layers]
         return K.concatenate(outputs, axis=-1)
+
+    def get_weights(self):
+        weights = []
+        for layer in self.layers:
+            weights += layer.get_weights()
+        return weights
+
+    def set_weights(self, weights):
+        offset = 0
+        for layer in self.layers:
+            length = len(layer.get_weights())
+            layer.set_weights(weights[offset:offset + length])
+            offset += length
+
+    @property
+    def trainable_weights(self):
+        weights = []
+        for layer in self.layers:
+            weights += layer.trainable_weights
+        return weights
+
+    @property
+    def non_trainable_weights(self):
+        weights = []
+        for layer in self.layers:
+            weights += layer.non_trainable_weights
+        return weights
+
+    @property
+    def updates(self):
+        updates = []
+        for layer in self.layers:
+            if hasattr(layer, 'updates'):
+                updates += layer.updates
+        return []
+
+    def get_updates_for(self, inputs=None):
+        inner_inputs = inputs
+        if inputs is not None:
+            uid = keras.utils.generic_utils.object_list_uid(inputs)
+            if uid in self._input_map:
+                inner_inputs = self._input_map[uid]
+
+        updates = []
+        for layer in self.layers:
+            layer_updates = layer.get_updates_for(inner_inputs)
+            layer_updates += super(MultiHead, self).get_updates_for(inputs)
+            updates += layer_updates
+        return updates
+
+    @property
+    def losses(self):
+        losses = []
+        for layer in self.layers:
+            if hasattr(layer, 'losses'):
+                losses += layer.losses
+        return losses
+
+    def get_losses_for(self, inputs=None):
+        if inputs is None:
+            losses = []
+            for layer in self.layers:
+                losses = layer.get_losses_for(None)
+            return losses + super(MultiHead, self).get_losses_for(None)
+        return super(MultiHead, self).get_losses_for(inputs)
--- tests/test_multi_head.py
+++ tests/test_multi_head.py
@@ -9,23 +9,71 @@ from keras_piecewise_pooling import PiecewisePooling1D
 from keras_multi_head import MultiHead
 
 
+class MaskFlatten(keras.layers.Flatten):
+
+    def __init__(self, **kwargs):
+        self.supports_masking = True
+        super(MaskFlatten, self).__init__(**kwargs)
+
+    def compute_mask(self, _, mask=None):
+        return mask
+
+
 class TestMultiHead(unittest.TestCase):
 
     def test_multi_attention(self):
         model = keras.models.Sequential()
         model.add(keras.layers.Embedding(input_dim=5, output_dim=3, mask_zero=True, name='Embed'))
         model.add(MultiHead(Attention(name='Attention'), layer_num=5, name='Multi-Head-Attention'))
+        model.add(keras.layers.TimeDistributed(MaskFlatten(), name='Flatten'))
+        model.add(keras.layers.Bidirectional(keras.layers.GRU(units=8), name='Bi-GRU'))
+        model.add(keras.layers.Dense(units=2, activation='softmax', name='Dense'))
         model.build()
-        model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.mae, metrics=[keras.metrics.mae])
+        model.compile(
+            optimizer=keras.optimizers.Adam(),
+            loss=keras.losses.sparse_categorical_crossentropy,
+            metrics=[keras.metrics.sparse_categorical_accuracy],
+        )
+
+        def data_generator(batch_size=32):
+            while True:
+                max_len = random.randint(5, 10)
+                data = np.zeros((batch_size, max_len))
+                tag = np.zeros(batch_size, dtype='int32')
+                for i in range(batch_size):
+                    datum_len = random.randint(1, max_len - 1)
+                    total = 0
+                    for j in range(datum_len):
+                        data[i, j] = random.randint(1, 4)
+                        total += data[i, j]
+                    tag[i] = total % 2
+                yield data, tag
+
+        model.fit_generator(
+            generator=data_generator(),
+            steps_per_epoch=100,
+            epochs=100,
+            validation_data=data_generator(),
+            validation_steps=10,
+            callbacks=[
+                keras.callbacks.EarlyStopping(monitor='val_loss', patience=2),
+            ],
+        )
+        model.layers[1].set_weights(model.layers[1].get_weights())
+
         model_path = os.path.join(tempfile.gettempdir(), 'test_save_load_%f.h5' % random.random())
         model.save(model_path)
         model = keras.models.load_model(model_path, custom_objects={
+            'MaskFlatten': MaskFlatten,
             'SeqSelfAttention': Attention,
             'MultiHead': MultiHead,
         })
         model.summary()
-        predicts = model.predict(np.random.randint(0, 4, (5, 7)))
-        self.assertEqual((5, 7, 3, 5), predicts.shape)
+        for data, tag in data_generator():
+            predicts = model.predict(data)
+            predicts = np.argmax(predicts, axis=-1)
+            self.assertGreaterEqual(np.sum(tag == predicts), 30)
+            break
 
     def test_fake_multi_pooling(self):
         data = [
