9befdbccaf0f136ac495d121e92b75815c710558
CyberZHG <CyberZHG@gmail.com>
2019-02-01 09:11:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-multi-head',
-    version='0.14.0',
-    packages=['keras_multi_head'],
+    version='0.15.0',
+    packages=find_packages(),
     url='https://github.com/CyberZHG/keras-multi-head',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='A wrapper layer for stacking layers horizontally',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'Keras',
-        'keras-self-attention==0.31.0',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
